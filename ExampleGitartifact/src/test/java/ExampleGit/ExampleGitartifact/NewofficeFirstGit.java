3b5a958775304ce95f1162230da161ce718c1314
akundurt <akundurt@GDCPHXDT118456.igategegdc.com>
2018-02-26 13:54:27 +0530

    Initial Checkin of Example Git Artifact

---
+++ ExampleGitartifact/src/test/java/ExampleGit/ExampleGitartifact/NewofficeFirstGit.java
@@ -0,0 +1,13 @@
+package ExampleGit.ExampleGitartifact;
+
+public class NewofficeFirstGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("FirstGit Export");
+	}
+
+}
